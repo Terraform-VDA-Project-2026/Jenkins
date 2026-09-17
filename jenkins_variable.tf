e6cc17f950c188560760d033e819a9990c021875
sureshkumarr2710-cell <sureshkumarr2710@gmail.com>
2026-09-17 14:35:38 +0000

    jenkins files updated

--- jenkins_variable.tf
+++ jenkins_variable.tf
@@ -1,20 +1,39 @@
-resource "azurerm_managed_disk" "mydisk" {
-  name                          = var.disk_name
-  location                      = var.my_location
-  resource_group_name           = var.resource_group_name
-  storage_account_type          = "Standard_LRS"
-  create_option                 = "Empty"
-  disk_size_gb                  = var.disk_size
-  public_network_access_enabled = var.public_network_access_enabled
+variable "disk_name" {
+  type        = string
+  description = "It is the name of managed disk"
+default = terraform_jenkins
+}
 
-  tags = var.tags
-
+variable "my_location" {
+  type    = string
+  default = "eastus"
 }
 
 
-resource "azurerm_virtual_network" "terra_vm_network" {
-  name                = "terra_vm_network_01"
-  resource_group_name = "terraform"
-  address_space       = var.vnet_address_space
-  location            = var.my_location
+variable "disk_size" {
+  type    = number
+  default = 1
+}
+
+variable "public_network_access_enabled" {
+  type    = bool
+}
+
+variable "vnet_address_space" {
+  type = list(string)
+  default = ["10.0.0.0/16","10.0.0.0/18", "10.0.0.0/20"]
+}
+
+
+variable "tags" {
+  type = map(string)
+  default = {
+    "environment" = "staging"
+    "project_id"  = "xyz0123"
+    "project_number" = 1223
+  }
+}
+
+variable "resource_group_name" {
+  type = string
 }
