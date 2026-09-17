d6a4a6b5c72b4a9720b4d2923d571c16dc15f61c
sureshkumarr2710-cell <sureshkumarr2710@gmail.com>
2026-09-17 14:22:15 +0000

    moving the jenkins file to folder

---
+++ jenkins_resource.tf
@@ -0,0 +1,20 @@
+resource "azurerm_managed_disk" "mydisk" {
+  name                          = var.disk_name
+  location                      = var.my_location
+  resource_group_name           = var.resource_group_name
+  storage_account_type          = "Standard_LRS"
+  create_option                 = "Empty"
+  disk_size_gb                  = var.disk_size
+  public_network_access_enabled = var.public_network_access_enabled
+
+  tags = var.tags
+
+}
+
+
+resource "azurerm_virtual_network" "terra_vm_network" {
+  name                = "terra_vm_network_01"
+  resource_group_name = "terraform"
+  address_space       = var.vnet_address_space
+  location            = var.my_location
+}
