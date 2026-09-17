4b53cd7b734cc8995a48b0250fdb708a724a8b52
sureshkumarr2710-cell <sureshkumarr2710@gmail.com>
2026-09-17 16:09:06 +0000

    change

--- provider.tf
+++ provider.tf
@@ -2,18 +2,13 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = " < 6.0"
+      version = "~> 4.0"
     }
   }
 }
 
-# Configure the Microsoft Azure Provider
 provider "azurerm" {
-
-features {}
-
-subscription_id = "252026ba-6735-4965-9bbf-6bad9c701532"
-
-tenant_id = "ddba09d2-5005-40ad-b8b5-6f7967a19bcf"
-
+  features {}
+  subscription_id = "252026ba-6735-4965-9bbf-6bad9c701532"
+  tenant_id       = "ddba09d2-5005-40ad-b8b5-6f7967a19bcf"
 }
