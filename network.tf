b3dbea8a55489f8df6d69753b72fc45ae972452e
sureshkumarr2710-cell <sureshkumarr2710@gmail.com>
2026-09-25 17:01:08 +0000

    dfds

--- network.tf
+++ network.tf
@@ -1,5 +1,5 @@
 resource "azurerm_resource_group" "name" {
-  name     = "jenrgs"
+  name     = "jenrg"
   location = "eastus"
 }
 
--- provider.tf
+++ provider.tf
@@ -3,7 +3,7 @@ terraform {
  backend "azurerm" {
   storage_account_name = "jenkinsstatefile"
   container_name = "state"
-  resource_group_name = "jenrgs"
+  resource_group_name = "jenrg"
   key = "terraform.tfstate"
   subscription_id = "252026ba-6735-4965-9bbf-6bad9c701532"
     
