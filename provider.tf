68dc3074b1828caa27a54697a90fa9f3f8e1401d
sureshkumarr2710-cell <sureshkumarr2710@gmail.com>
2026-09-25 16:56:40 +0000

    dsdw

--- provider.tf
+++ provider.tf
@@ -1,10 +1,11 @@
 terraform {
 
  backend "azurerm" {
-  storage_account_name = "jenkinsstatefile "
+  storage_account_name = "jenkinsstatefile"
   container_name = "state"
   resource_group_name = "jenrgs"
   key = "terraform.tfstate"
+  subscription_id = "252026ba-6735-4965-9bbf-6bad9c701532"
     
  }
   required_providers {
